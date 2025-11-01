63859dc7c3fa8c9e440756262b3ecc50ac025eab
Marcos <Kampusam@gmail.com>
2025-11-01 17:08:52 -0300

    testa a classe Livro

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
 require_once('vendor/autoload.php');
 
+use Marcos\Biblioteca\Livro;
 
+echo 'sistama de biblioteca inciciado <br>';
+
+$livro = new Livro("samuel", "teste1");
+echo '<pre>';
+
+echo 'Livro: '. $livro->getTitulo() . '<br>';
+echo 'Autor: '. $livro->getAutor() . '<br>';
+echo 'Disponibilidade: '. ($livro->estaDisponivel() ? 'sim' : 'nao') . '<br>';
 
 ?>
