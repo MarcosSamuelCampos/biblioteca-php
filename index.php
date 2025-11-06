a78cf923ae9a6fdd207d3c0e2a8ca1d4c1b51c29
Marcos <Kampusam@gmail.com>
2025-11-06 19:50:21 -0300

    test(estante): testar o método listarLivrosDisponiveis()

--- index.php
+++ index.php
@@ -8,15 +8,16 @@ echo 'sistama de biblioteca inciciado <br>';
 
 $livro1 = new Livro("samuel", "teste1");
 $livro2 = new Livro("samuel2", "teste12");
+$livro1->marcarDisponivel();
 echo '<pre>';
 
 $estante = new Estante();
 
 $estante->adicionarLivro($livro1);
 $estante->adicionarLivro($livro2);
-echo '<pre>';
-print_r($estante);
+
 echo '<hr>';
-$estante->removerLivro($livro1);
-print_r($estante);
+
+$LivroEncontrado = $estante->buscarLivroPorTitulo("teste");
+var_dump($estante->listarLivrosDisponiveis());
 ?>
