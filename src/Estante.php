2e72dfdbf19bafa624718e503dab83fb4ab12f53
Marcos <Kampusam@gmail.com>
2025-11-06 17:10:50 -0300

    feat(estante): adicionar método removerLivro() para remover objetos Livro da estante

--- src/Estante.php
+++ src/Estante.php
@@ -6,10 +6,27 @@ class Estante{
 
     private array $livros = [];
 
-    public function adicionarLivro(Livro $livro):void{
+    public function adicionarLivro(Livro $livro):void
+    {
      $this->livros[] = $livro;
     }
-     
+     public function removerLivro(Livro $livro):void
+     {
+        $this->livros = array_filter(
+            //função anônima;
+            $this->livros, function($livroAtual) use ($livro){
+
+                echo 'livro atual: ' . $livroAtual->getTitulo();
+                if ($livroAtual === $livro) {
+                    //Se for o mesmo livro, imprime a mensagem “livro removido”
+                    echo '- livro removido';
+                   
+                }
+                 echo '<br>';
+                return $livroAtual !== $livro;
+            }
+        );
+     }
 }
 
 ?>
