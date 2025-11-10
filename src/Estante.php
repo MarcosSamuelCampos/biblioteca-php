82aa258bb85da87a8ea2392927287d52710d2d25
Marcos <Kampusam@gmail.com>
2025-11-10 15:43:03 -0300

    refactor(Estante) : removendo codigos desnecessários - echos

--- src/Estante.php
+++ src/Estante.php
@@ -11,16 +11,10 @@ class Estante{
     }
      public function removerLivro(Livro $livro):void
      {
-        $this->livros = array_filter(
-            //função anônima;
-            $this->livros, function($livroAtual) use ($livro)
+        $this->livros = array_filter(    //função anônima;
+            $this->livros,
+             function($livroAtual) use ($livro)
             {
-                echo 'livro atual: ' . $livroAtual->getTitulo();
-                if ($livroAtual === $livro) {
-                    //Se for o mesmo livro, imprime a mensagem “$livro - livro removido”
-                    echo '- livro removido';
-                }
-                 echo '<br>';
                 return $livroAtual !== $livro;
             }
         );
